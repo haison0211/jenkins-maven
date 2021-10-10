0b15d012e6c506c3df3ca25fe017249e21ea864d
haison0211 <haison.le286@email.com>
2021-10-10 13:35:35 +0000

    xcvawr

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,7 +2,7 @@
 
 echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
-#echo $PASS >> /tmp/.auth
+echo $PASS >> /tmp/.auth
 
 scp -i /opt/vm-deploy.pem /tmp/.auth ubuntu@18.166.116.124:/tmp/.auth
 scp -i /opt/vm-deploy.pem /home/ubuntu/pipeline/jenkins/deploy/publish.sh ubuntu@18.166.116.124:/home/ubuntu/publish.sh
