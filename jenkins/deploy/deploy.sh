928a9e5afe663b4fae491d08aa505371f05e6a78
haison0211 <haison.le286@email.com>
2021-10-10 13:26:34 +0000

    c

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,9 +2,9 @@
 
 echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
-echo $PASS >> /tmp/.auth
+#echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth ubuntu@18.166.116.124:/tmp/.auth
-scp -i /opt/prod /home/ubuntu/pipeline/jenkins/deploy/publish.sh ubuntu@18.166.116.124:/home/ubuntu/publish.sh
-ssh -i /opt/prod ubuntu@18.166.116.124 "/home/ubuntu/publish.sh"
+scp -i /opt/vm-deploy.pem /tmp/.auth ubuntu@18.166.116.124:/tmp/.auth
+scp -i /opt/vm-deploy.pem /home/ubuntu/pipeline/jenkins/deploy/publish.sh ubuntu@18.166.116.124:/home/ubuntu/publish.sh
+ssh -i /opt/vm-deploy.pem ubuntu@18.166.116.124 "/home/ubuntu/publish.sh"
 
